7e79a845125f317ed48e09d1b9ac4d1f67a48bd8
Shania Lopez <slopez@fandm.edu>
2023-04-24 18:23:13 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const newTaskDescription = document.getElementById("new-task-description")
+    buildToDo(newTaskDescription.value)
+  })
 });
+
+function buildToDo(toDo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deleteToDo)
+  btn.textContent = 'x'
+  p.textContent = `${toDo} `
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function deleteToDo(e) {
+  e.target.parentNode.remove()
+}
+
+function createDropdown() {
+  let sel = document.createElement('select')
+  let opt = document.createElement('option')
+  document.querySelector('p').appendChild(sel)
+  document.querySelector('select').appendChild(opt)
+}
